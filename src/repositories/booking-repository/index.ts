0fd023363cb884b59f6526b10a8bc491756c7703
Otavio Goulart <otaviobgoulartt@gmail.com>
2023-02-10 15:01:24 -0300

    fix: include room on getUserBooking, feat: getUserBooking service

--- src/repositories/booking-repository/index.ts
+++ src/repositories/booking-repository/index.ts
@@ -6,6 +6,9 @@ async function getUserBooking(userId: number) {
     return prisma.booking.findFirst({
         where: {
             userId
+        },
+        include: {
+            Room: true
         }
     })
 }
--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -1,7 +1,14 @@
+import { notFoundError } from "@/errors"
+import { bookingRepository } from "@/repositories/booking-repository"
 
 
+async function getUserBooking(userId: number) {
+    const booking = await bookingRepository.getUserBooking(userId)
+
+    if (!booking) throw notFoundError()
+}
 async function getBooking(userId: number) {
-
+    await
 }
 
 export const bookingService = {
