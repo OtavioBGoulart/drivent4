906c587b99e76e96b485b43eac85e6a0eea92a02
Otavio Goulart <otaviobgoulartt@gmail.com>
2023-02-10 15:35:30 -0300

    feat: createBooking controller

--- src/controllers/bookings-controller.ts
+++ src/controllers/bookings-controller.ts
@@ -16,3 +16,18 @@ export async function getBooking(req: AuthenticatedRequest, res: Response) {
         return res.sendStatus(httpStatus.BAD_REQUEST);
     }
 }
+
+export async function createBooking(req: AuthenticatedRequest, res: Response) {
+    const { userId } = req;
+    const { roomId } = req.body;
+
+    try {
+        const booking = await bookingService.createBooking(userId, roomId)
+        return res.sendStatus(httpStatus.CREATED)
+    } catch (error) {
+        if (error.name === "NotFoundError") {
+            return res.sendStatus(httpStatus.NOT_FOUND);
+        }
+        return res.sendStatus(httpStatus.BAD_REQUEST);
+    }
+}
